b3a79aab960670bfd32b04e6188a86dd168a3da5
Anton <stells@ngs.ru>
2021-03-21 10:20:26 +0300

    final release poject

--- project/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore, applyMiddleware} from 'redux'
-import authReducer from '../redux/reducer.js'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import thunk from 'redux-thunk';
-
-export const store = createStore(authReducer, composeWithDevTools(applyMiddleware(thunk)))
